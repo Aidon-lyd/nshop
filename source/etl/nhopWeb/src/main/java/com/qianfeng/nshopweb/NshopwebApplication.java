5ff811293a2df3722a3102ee2cd15bb8ed284a00
licsman <licsman@163.com>
2019-09-29 14:49:19 +0800

    14:49前端重建

---
+++ source/etl/nhopWeb/src/main/java/com/qianfeng/nshopweb/NshopwebApplication.java
@@ -0,0 +1,13 @@
+package com.qianfeng.nshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NshopwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NshopwebApplication.class, args);
+    }
+
+}
